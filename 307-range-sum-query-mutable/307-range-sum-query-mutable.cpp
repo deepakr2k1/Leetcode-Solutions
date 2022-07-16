a3e02d7282b95ddab929395afa607b39e71322bc
Deepak Rathore <59319090+deepakrathore2k1@users.noreply.github.com>
2022-07-16 14:34:24 +0530

    Time: 683 ms (50.22%), Space: 150.9 MB (70.04%) - LeetHub

--- 307-range-sum-query-mutable/307-range-sum-query-mutable.cpp
+++ 307-range-sum-query-mutable/307-range-sum-query-mutable.cpp
@@ -1,10 +1,16 @@
 #define lc idx<<1
 #define rc (idx<<1)|1
-
+/*
+segment tree - struct default values, combine
+lazy - pushUpdate
+*/
 class SegmentTree {
 public:
     struct Node {
-        int val;
+        // TODO: default values
+        int val = 0;
+        bool isPending = false;
+        int pendingUpdate = 0;
     };
     int n;
     vector<Node> t;
@@ -15,66 +21,104 @@ public:
         n = A.size();
         t.resize(n << 2);
         build(1, 0, n - 1, A);
-        empty.val = 0;
     }
 
-    // combine
-    Node combine(Node& l, Node& r) {
-        Node ans;
-        ans.val = l.val + r.val;
-        return ans;
+    // TODO: combine
+    void combine(Node& par, Node& left_c, Node& right_c) {
+        par.val = left_c.val + right_c.val;
+    }
+
+    // TODO: push update
+    void pushUpdate(int idx, int start, int end) {
+        t[idx].val = min(t[idx].val, t[idx].pendingUpdate);
+        t[idx].isPending = false;
+        if(start == end) return;
+        t[lc].pendingUpdate = min(t[lc].pendingUpdate, t[idx].pendingUpdate);
+        t[rc].pendingUpdate = min(t[rc].pendingUpdate, t[idx].pendingUpdate);
+        t[lc].isPending = true;
+        t[rc].isPending = true;
     }
 
     // build segment tree
-    void build(int idx, int l, int r, vector<int>& A) {
-        if(l > r) return;
-        if(l == r) {                                // leaf node
-            t[idx].val = A[l];
+    void build(int idx, int start, int end, vector<int>& A) {
+        if(end > end) return;
+        if(start == end) {                                  // leaf node
+            t[idx].val = A[start];
             return;
         }
-        int mid = (l + r) / 2;
-        build(lc, l, mid, A);
-        build(rc, mid + 1, r, A);
-        t[idx] = combine(t[lc], t[rc]);
+        int mid = (start + end) / 2;
+        build(lc, start, mid, A);
+        build(rc, mid + 1, end, A);
+        combine(t[idx], t[lc], t[rc]);
     }
 
     // Query
     Node query(int idx, int start, int end, int ql, int qr) {
-
-        if(ql > end || qr < start)                  // No overlap
-            return empty;
-        if(ql <= start && qr >= end)                // Complete query
-            return t[idx];
-        int mid = (start + end) / 2;                // Partial Overlap
+        if(ql > end || qr < start) return empty;                // No overlap
+        if(t[idx].isPending) pushUpdate(idx, start, end);       // pending update
+        if(ql <= start && qr >= end) return t[idx];             // Complete query
+        int mid = (start + end) / 2;
         Node left_ans = query(lc, start, mid, ql, qr);
         Node right_ans = query(rc, mid + 1, end, ql, qr);
-        return combine(left_ans, right_ans);
+        Node ans;
+        combine(ans, left_ans, right_ans);
+        return ans;
     }
     int query(int ql, int qr) {
+        // TODO:
         Node ans = query(1, 0, n - 1, ql, qr);
         return ans.val;
     }
 
     // Update
-    void update(int idx, int start, int end, int i, Node newNode) {
-        if(i<start || i>end) return;                // No overlap
-        if(start == end) {                          // Leaf-node
+    void update(int idx, int start, int end, int index, Node newNode) {
+        if(index<start || index>end) return;                    // No overlap
+        if(t[idx].isPending) pushUpdate(idx, start, end);       // pending update
+        if(start == end) {                                      // Leaf-node
             t[idx] = newNode;
             return;
         }
         int mid = (start + end) / 2;
-        update(lc, start, mid, i, newNode);
-        update(rc, mid + 1, end, i, newNode);
-        t[idx] = combine(t[lc], t[rc]);
-        return;
+        update(lc, start, mid, index, newNode);
+        update(rc, mid + 1, end, index, newNode);
+        combine(t[idx], t[lc], t[rc]);
     }
-    void update(int i, int val) {
+    void update(int index, int val) {
+        // TODO:
         Node newNode;
         newNode.val = val;
-        update(1, 0, n - 1, i, newNode);
+        update(1, 0, n - 1, index, newNode);
     }
 
+    // Lazy update - range update
+    void updateRange(int idx, int start, int end, int ql, int qr, int val) {
+        if(start > end || start > qr || end < ql) return;       // No overlap
+        if(t[idx].isPending) pushUpdate(idx, start, end);       // pending update
+        if(start >= ql and end <= qr) {                         // complete update
+            // TODO: pendingUpdate
+            t[idx].pendingUpdate = val;
+            t[idx].isPending = true;
+            return;
+        }
+        int mid = (start + end) / 2;
+        updateRange(lc, start, mid, ql, qr, val);               // Updating left child
+        updateRange(rc, mid + 1, end, ql, qr, val);             // Updating right child
+        combine(t[idx], t[lc], t[rc]);
+    }
+    void updateRange(int ql, int qr, int val) {
+        updateRange(1, 0, n - 1, ql, qr, val);
+    }
+
+    void print() {
+        for(int i = 0; i < 4 * n; i++) {
+            cout << i << " " << t[i].val << endl;
+            cout << i << " " << (t[i].isPending ? 1 : 0) << endl;
+            cout << i << " " << t[i].pendingUpdate << endl;
+        }
+    }
 };
+
+
 vector<int> A(30004);
 SegmentTree tree(A);
 class NumArray {   
